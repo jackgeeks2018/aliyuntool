cb0f6863eccc0d42db641be080b6a47961eadd84
wpp <1315423739@qq.com>
2020-01-12 16:26:28 +0800

    阿里云工具

---
+++ src/test/java/com/aliyun/wpp/aliyun/AliyunApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aliyun.wpp.aliyun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AliyunApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
